024a2926497caeb9ab89216f780d1753cc414926
David Roig <DRoigHeredia@gmail.com>
2021-12-15 18:40:53 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrudComponent } from './private/crud/crud.component';
+import { DashboardComponent } from './private/dashboard/dashboard.component';
+import { GaleriaComponent } from './private/galeria/galeria.component';
+import { ProfileComponent } from './private/profile/profile.component';
+import { AboutComponent } from './public/about/about.component';
+import { HomeComponent } from './public/home/home.component';
+import { LoginComponent } from './public/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'galeria', component: GaleriaComponent },
+  { path: 'crud', component: CrudComponent },
+  { path: 'profile', component: ProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PublicNavbarComponent } from './layout/public-navbar/public-navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+import { HomeComponent } from './public/home/home.component';
+import { LoginComponent } from './public/login/login.component';
+import { AboutComponent } from './public/about/about.component';
+import { PrivateNavbarComponent } from './layout/private-navbar/private-navbar.component';
+import { DashboardComponent } from './private/dashboard/dashboard.component';
+import { GaleriaComponent } from './private/galeria/galeria.component';
+import { CrudComponent } from './private/crud/crud.component';
+import { ProfileComponent } from './private/profile/profile.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicNavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    AboutComponent,
+    PrivateNavbarComponent,
+    DashboardComponent,
+    GaleriaComponent,
+    CrudComponent,
+    ProfileComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
